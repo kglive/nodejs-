f9cfa0e476ff40254244b0c763ec9ffea4ef493a
kglive103945@163.com <ykjy@5137022>
2019-10-14 22:32:49 +0800

    登录模板

---
+++ 第七课 登录模板/app.js
@@ -0,0 +1,65 @@
+const Express = require('express');
+const path = require('path');
+const app = Express();
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+// 设置静态目录  
+app.use(Express.static(path.join(__dirname, '/public')));
+
+
+app.set('views', path.join(__dirname, 'views'));
+// app.engine('handlebars', exphbs());
+app.engine('hbs', exphbs.create({
+  // 布局目录是相对于 setting views 目录的 layouts 目录 （默认）
+  layoutsDir: '',
+  // 配置布局主文件
+  defaultLayout: 'main',
+  // 修改模板文件后缀名；后缀名修改之后，对应的engine名称也要跟着改变
+  extname: '.hbs',
+  // 配置部分模板目录 相对于 setting views 目录的 partials 目录 （默认）
+  partialsDir: '',
+  // 段落使用
+  helpers: {
+    section (name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+}).engine);
+
+app.set('view engine', 'hbs');
+
+
+// 解析网络请求参数 parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// application/json
+app.use(bodyParser.json());
+
+
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'welcome my site！' });
+})
+
+app.get('/login', (req, res) => {
+  res.render('login', { title: '用户登录'});
+})
+
+
+// 404页面
+app.use((req, res, next) => {
+  res.type('text/plain').status(404).send('404-Not Found');
+  next();
+})
+
+// 500
+app.use((error, req, res, next) => {
+  console.log('服务器报错', error);
+  return res.status(500).send('500 - Server Error');
+})
+
+app.listen(5000, () => {
+  console.log('Server Start on http://127.0.0.1:5000')
+})
